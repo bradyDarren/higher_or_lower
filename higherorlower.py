cc61fd24dcb08f7c9852f8972cc6eb99d190731b
bradyDarren <dcgbfinancial@gmail.com>
2024-10-18 18:10:33 -0500

    adjustment to the rand_account function and start function

--- higherorlower.py
+++ higherorlower.py
@@ -3,19 +3,25 @@ from art import logo, vs
 from data import ig_dict
 
 # takes list input and returns a data associated with a random selection from the input list.
+# the output will include the following the name of the instagram account, a description of what is displayed on the account
+# and where is the person, place, or thing from that owns the account.
 def rand_account(data_set):
     rand_selection = random.choice(data_set)
     # print(rand_selection["name"]) - test line
     name = rand_selection["name"]
     description = rand_selection["description"]
     country = rand_selection["country"]
-    return name, description, country
+    return f"{name}, {description}, from {country}"
 
-print(", ".join((rand_account(ig_dict))))
+def start():
+    print(logo)
+    correct = True
+    while correct:
+        comp_a = rand_account(ig_dict)
+        comp_b = rand_account(ig_dict)
+        print(f"Compare A: {comp_a}.")
+        print(vs)
+        print(f"Against B: {comp_b}.")
+        break
 
-# def start():
-#     print(logo)
-#     correct = True
-#     while correct: 
-#         print(f"Compare A: {rand_account(ig_dict)}.")
-
+start()
