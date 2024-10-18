ca5b4a9ba62ecbed1c34143c0326af931d1a6b2d
bradyDarren <dcgbfinancial@gmail.com>
2024-10-18 14:33:15 -0500

    addition of the data to be used within our game

---
+++ data.py
@@ -0,0 +1,158 @@
+ig_dict = [
+    {"name": "Cristiano Ronaldo", 
+     "country": "Portugal", 
+     "description": "Footballer sharing personal life and sponsorship posts", 
+     "follower count": 600},
+
+    {"name": "Lionel Messi", 
+     "country": "Argentina", 
+     "description": "Footballer sharing sports highlights and personal life", 
+     "follower count": 490},
+
+    {"name": "Selena Gomez", 
+     "country": "United States", 
+     "description": "Singer, actress, and mental health advocate", 
+     "follower count": 430},
+
+    {"name": "Kylie Jenner", 
+     "country": "United States", 
+     "description": "Entrepreneur and reality TV star with lifestyle and fashion content", 
+     "follower count": 400},
+
+    {"name": "Dwayne 'The Rock' Johnson", 
+     "country": "United States", 
+     "description": "Actor and former wrestler, focusing on fitness and motivational content", 
+     "follower count": 396},
+
+    {"name": "Kim Kardashian", 
+     "country": "United States", 
+     "description": "Reality TV star, entrepreneur, and influencer", 
+     "follower count": 380},
+
+    {"name": "Ariana Grande", 
+     "country": "United States", 
+     "description": "Singer and actress", 
+     "follower count": 370},
+
+    {"name": "Beyoncé", 
+     "country": "United States", 
+     "description": "Musician, producer, and actress", 
+     "follower count": 300},
+
+    {"name": "Khloé Kardashian", 
+     "country": "United States", 
+     "description": "Reality TV star and entrepreneur", 
+     "follower count": 315},
+
+    {"name": "Justin Bieber", 
+     "country": "Canada", 
+     "description": "Singer and musician", 
+     "follower count": 290},
+
+    {"name": "Kendall Jenner", 
+     "country": "United States", 
+     "description": "Model and reality TV star", 
+     "follower count": 292},
+
+    {"name": "Taylor Swift", 
+     "country": "United States", 
+     "description": "Singer and songwriter", 
+     "follower count": 280},
+
+    {"name": "Nike", 
+     "country": "United States", 
+     "description": "Sportswear brand sharing fitness, athletes, and products", 
+     "follower count": 320},
+
+    {"name": "National Geographic", 
+     "country": "United States", 
+     "description": "Media organization focusing on nature, science, and culture", 
+     "follower count": 280},
+
+    {"name": "Virat Kohli", 
+     "country": "India", 
+     "description": "Cricketer sharing sports updates and personal life", 
+     "follower count": 260},
+
+    {"name": "Jennifer Lopez", 
+     "country": "United States", 
+     "description": "Singer, actress, and dancer", 
+     "follower count": 250},
+
+    {"name": "Kourtney Kardashian", 
+     "country": "United States", 
+     "description": "Reality TV star and entrepreneur", 
+     "follower count": 220},
+
+    {"name": "Real Madrid CF", 
+     "country": "Spain", 
+     "description": "Football club sharing team updates and match highlights", 
+     "follower count": 162},
+
+    {"name": "Rihanna", 
+     "country": "Barbados", 
+     "description": "Singer and entrepreneur focusing on beauty, fashion, and music", 
+     "follower count": 150},
+
+    {"name": "Billie Eilish", 
+     "country": "United States", 
+     "description": "Singer and songwriter with personal and mental health-focused content", 
+     "follower count": 110},
+
+    {"name": "Shakira", 
+     "country": "Colombia", 
+     "description": "Musician and philanthropist", 
+     "follower count": 90},
+
+    {"name": "Priyanka Chopra", 
+     "country": "India", 
+     "description": "Actress and producer sharing a mix of Bollywood, Hollywood, and personal life", 
+     "follower count": 91},
+
+    {"name": "Dua Lipa", 
+     "country": "United Kingdom", 
+     "description": "Singer-songwriter with posts on her music, fashion, and lifestyle", 
+     "follower count": 88},
+
+    {"name": "David Beckham", 
+     "country": "United Kingdom", 
+     "description": "Former footballer, sharing family life and sports-related content", 
+     "follower count": 88},
+
+    {"name": "Chris Brown", 
+     "country": "United States", 
+     "description": "Singer and dancer with content on music, dance, and art", 
+     "follower count": 145},
+
+    {"name": "Drake", 
+     "country": "Canada", 
+     "description": "Rapper and entrepreneur sharing insights into his career and personal life", 
+     "follower count": 146},
+
+    {"name": "FC Barcelona", 
+     "country": "Spain", 
+     "description": "Football club with updates on matches and players", 
+     "follower count": 130},
+
+    {"name": "UEFA Champions League", 
+     "country": "Europe", 
+     "description": "Football competition posting match highlights and behind-the-scenes content", 
+     "follower count": 115},
+
+    {"name": "Gal Gadot", 
+     "country": "Israel", 
+     "description": "Actress known for her role as Wonder Woman, sharing film and advocacy content", 
+     "follower count": 108},
+
+    {"name": "Vin Diesel", 
+     "country": "United States", 
+     "description": "Actor famous for the 'Fast & Furious' series, sharing personal and motivational posts", 
+     "follower count": 102}
+]
+
+
+count = 0 
+
+for x in ig_dict:
+    count += 1
+print(count)    
