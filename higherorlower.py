60e75ad203c691f211bd7c994885a4513941b78c
bradyDarren <dcgbfinancial@gmail.com>
2024-10-19 09:59:04 -0500

    moved the calling of the rand_account function for account b into the while loop so it can be called repeatedly

--- higherorlower.py
+++ higherorlower.py
@@ -2,19 +2,27 @@ import random
 from art import logo, vs
 from data import ig_dict
 
-# takes list input and returns a data associated with a random selection from the input list.
+# takes list input and returns a list associated with a random selection from the input list.
 # the output will include the following the name of the instagram account, a description of what is displayed on the account
 # and where is the person, place, or thing from that owns the account.
 def rand_account(data_set):
     rand_selection = random.choice(data_set)
-    # print(rand_selection["name"]) - test line
+    #  - test line
+    # print(rand_selection["name"])
     name = rand_selection["name"]
     description = rand_selection["description"]
     country = rand_selection["country"]
     follower_count = rand_selection["follower count"]
     return [name, description, country, follower_count]
 
-print(rand_account(ig_dict)[0])
+# - test line
+# print(rand_account(ig_dict)[0])
+
+account1 = rand_account(ig_dict)[3]
+print(account1)
+account2 = rand_account(ig_dict)[3]
+print(account2)
+print(max(account1,account2))
 
 def start():
     print(logo)
@@ -27,6 +35,7 @@ def start():
         print(comp_a)
         print(vs)
         print(comp_b)
-        break
+        user_selection = input("Which of the above meantioned has more followers? Type 'A' or 'B': ").lower()
 
-start()
+        
+# start()
